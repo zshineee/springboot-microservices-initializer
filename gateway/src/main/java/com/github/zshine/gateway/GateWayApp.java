f40902ef942217b2da940178a737b2e9d86087e2
zshineee <96932537+zshineee@users.noreply.github.com>
2022-11-17 14:56:47 +0800

    feat(gateway):
    新增网关

---
+++ gateway/src/main/java/com/github/zshine/gateway/GateWayApp.java
@@ -0,0 +1,29 @@
+package com.github.zshine.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@SpringBootApplication
+@RestController
+@EnableDiscoveryClient
+public class GateWayApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayApp.class);
+    }
+
+
+    @GetMapping("/health")
+    public Mono<String> health() {
+        return Mono.create(stringMonoSink -> stringMonoSink.success("health check"));
+    }
+
+
+}
