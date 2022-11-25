e517978d99573130c43b24da64605f819c8a30be
zshineee <zshineeeee@gmail.com>
2022-11-25 22:05:42 +0800

    refactor(web-admin): 去除ngzorro初始化

--- web-admin/src/app/app.module.ts
+++ web-admin/src/app/app.module.ts
@@ -2,15 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NZ_I18N } from 'ng-zorro-antd/i18n';
-import { zh_CN } from 'ng-zorro-antd/i18n';
-import { registerLocaleData } from '@angular/common';
-import zh from '@angular/common/locales/zh';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// import { NZ_I18N } from 'ng-zorro-antd/i18n';
+// import { zh_CN } from 'ng-zorro-antd/i18n';
+// import { registerLocaleData } from '@angular/common';
+// import zh from '@angular/common/locales/zh';
+// import { FormsModule } from '@angular/forms';
+// import { HttpClientModule } from '@angular/common/http';
+// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-registerLocaleData(zh);
+// registerLocaleData(zh);
 
 @NgModule({
   declarations: [
@@ -18,12 +18,12 @@ registerLocaleData(zh);
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpClientModule,
-    BrowserAnimationsModule
+    // FormsModule,
+    // HttpClientModule,
+    // BrowserAnimationsModule
   ],
   providers: [
-    { provide: NZ_I18N, useValue: zh_CN }
+    // { provide: NZ_I18N, useValue: zh_CN }
   ],
   bootstrap: [AppComponent]
 })
