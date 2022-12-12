da3c65a853d9a0cc49ed54e9ef40c69e6fa9b33a
zshineee <96932537+zshineee@users.noreply.github.com>
2022-12-12 16:56:01 +0800

    feat(web-admin/user): 新增用户查询和删除功能

--- web-admin/src/app/user/user.component.ts
+++ web-admin/src/app/user/user.component.ts
@@ -1,4 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {BaseJsonRsp} from "../common/domain/BaseJsonRsp";
+import {environment} from "../../environments/environment";
+import {PageJsonRsp} from "../common/domain/PageJsonRsp";
+import {HttpClient} from "@angular/common/http";
+import {NzModalService} from "ng-zorro-antd/modal";
+import {NzMessageService} from "ng-zorro-antd/message";
+
+export interface User {
+  username: string;
+  fullname: string;
+  remark: string;
+  status: number;
+  //status string类型
+  statusString: string;
+  //status 说明
+  statusRemark: string;
+  supper: number;
+  //supper string类型
+  supperString: string;
+  // supper 说明
+  supperRemark: string;
+}
+
 
 @Component({
   selector: 'app-user',
@@ -7,9 +30,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
+  dataList: User[] = [];
+  page = 1;
+  limit = 10;
+  total = 0;
+
+
+  constructor(private http: HttpClient, private modal: NzModalService, private message: NzMessageService) {
+
+  }
+
 
   ngOnInit(): void {
+  }
+
+  query(page: number, limit: number): void {
+    this.http
+      .get<PageJsonRsp<User>>(environment.contextPath + "auth/user/page", {
+        params: {
+          page: page,
+          limit: limit
+        }
+      })
+      .subscribe(pageData => {
+        pageData.data.map(data => {
+          data.statusRemark = data.status == 1 ? "生效" : "失效"
+          data.supperRemark = data.supper == 1 ? "是" : "否"
+        });
+        this.dataList = pageData.data;
+        this.page = page;
+        this.limit = limit;
+        this.total = pageData.total;
+      });
+  }
+
+  delete(username: string): void {
+    this.modal.confirm({
+      nzTitle: '确定要删除吗?',
+      nzOkText: '确定',
+      nzOkType: 'primary',
+      nzOkDanger: true,
+      nzOnOk: () => {
+        this.http.delete<BaseJsonRsp>(environment.contextPath + "auth/user/delete", {body: {username: username}})
+          .subscribe((data) => {
+            if (data.success) {
+              this.query(this.page, this.limit)
+            } else {
+              this.message.create("error", data.errMsg)
+            }
+          })
+      },
+      nzCancelText: '取消',
+    });
+  }
+
+  edit(param: User): void {
+
+  }
+
+  add(): void {
+
   }
 
 }
--- web-admin/src/app/user/user.module.ts
+++ web-admin/src/app/user/user.module.ts
@@ -3,13 +3,33 @@ import { CommonModule } from '@angular/common';
 
 import { UserRoutingModule } from './user-routing.module';
 import {UserComponent} from "./user.component";
+import {ReactiveFormsModule} from "@angular/forms";
+import {NzFormModule} from "ng-zorro-antd/form";
+import {NzSelectModule} from "ng-zorro-antd/select";
+import {NzCardModule} from "ng-zorro-antd/card";
+import {NzTableModule} from "ng-zorro-antd/table";
+import {NzButtonModule} from "ng-zorro-antd/button";
+import {NzIconModule} from "ng-zorro-antd/icon";
+import {NzDividerModule} from "ng-zorro-antd/divider";
+import {NzModalModule} from "ng-zorro-antd/modal";
+import {NzMessageModule} from "ng-zorro-antd/message";
 
 
 @NgModule({
   declarations: [UserComponent],
   imports: [
     CommonModule,
-    UserRoutingModule
+    UserRoutingModule,
+    ReactiveFormsModule,
+    NzFormModule,
+    NzSelectModule,
+    NzCardModule,
+    NzTableModule,
+    NzButtonModule,
+    NzIconModule,
+    NzDividerModule,
+    NzModalModule,
+    NzMessageModule
   ]
 })
 export class UserModule { }
